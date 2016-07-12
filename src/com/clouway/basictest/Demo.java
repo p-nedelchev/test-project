b7ad5707fdd7c6445024a41850c63f5d2c8ee434
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-07-12 14:18:06 +0300

    tp: Basic output Adds a new file with basic output

---
+++ src/com/clouway/basictest/Demo.java
@@ -0,0 +1,10 @@
+package com.clouway.basictest;
+
+/**
+ * @author Petar Nedelchev (peter.krasimirov@gmail.com)
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+    }
+}

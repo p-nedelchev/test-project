69ce755fda5a2cffacb915efb2b161d38cee3537
Petar Nedelchev <peter.krasimirov@gmail.com>
2016-07-13 13:19:00 +0300

    tp: Array from integers added and printed
    Adds array or 10 interger numbers than prints them

--- src/com/clouway/basicreader/Demo.java
+++ src/com/clouway/basicreader/Demo.java
@@ -11,5 +11,14 @@ public class Demo {
         System.out.println("Enter your name:");
         String name = sc.next();
         System.out.println("Your name is " + name);
+        int [] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
     }
 }
